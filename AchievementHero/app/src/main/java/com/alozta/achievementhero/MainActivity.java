14813621c44d1039fcebbc7e0cc50715b2583bf2
alozta <alozta@gmail.com>
2016-08-09 20:28:45 +0300

    Main activity

---
+++ AchievementHero/app/src/main/java/com/alozta/achievementhero/MainActivity.java
@@ -0,0 +1,10 @@
+package com.alozta.achievementhero;
+
+import android.app.Activity;
+
+/**
+ * Created by alozta on 8/9/16.
+ */
+public class MainActivity extends Activity {
+
+}
---
+++ AchievementHero/app/src/main/java/com/alozta/achievementhero/WelcomeActivity.java
@@ -0,0 +1,28 @@
+package com.alozta.achievementhero;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.alozta.achievementhero.R;
+
+/**
+ * Created by alozta on 8/8/16.
+ */
+public class WelcomeActivity extends Activity {
+    Button startButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+
+        startButton = (Button)findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // CALL FOR MAIN ACTIVITY
+            }
+        });
+    }
+}
